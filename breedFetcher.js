0776b03aefc8c108512c470ce8bbc7c9433273ec
Bazil <king.bazil.khan@gmail.com>
2022-05-30 08:46:59 +0000

    refactored, add mocha tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
+
 const request = require('request');
-const breedSearch = process.argv[2];
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedSearch;
-
-request(url, function(error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  if (body.length === 2)  {
-    console.log('No data for requested breed.');
-  } else  {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, function(error, body) {
+    if (body.body.length == 2)  {
+      return callback('did not find that cat', null)
+    } else  {
+      // console.log(body.body)
+      const data = JSON.parse(body.body);
+      return callback(null, data[0].description);
+    }
   }
-});
+)}
+
+module.exports = { fetchBreedDescription };
